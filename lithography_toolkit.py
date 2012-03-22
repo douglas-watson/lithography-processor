324eb949a824f65df9ad636dd6265781992f7d4b
Douglas Watson <douglas@watsons.ch>
2012-03-22 14:36:49 -0400

    Extended the toolkit for extraction of black pixels and coordinate transform.

--- lithography_toolkit.py
+++ lithography_toolkit.py
@@ -24,7 +24,7 @@ the user interface.
 '''
 
 import Image
-from numpy import array, asarray, cross
+from numpy import array, asarray, cross, linspace, meshgrid
 from numpy.linalg import norm
 
 def path_to_array(image_path):
@@ -68,10 +68,67 @@ def get_referential(O, A, B):
 
     return eX, eY, eZ
 
+def get_black_points(data, width, height):
+    ''' Returns the x and y coordinates of all black pixels in the image.
+
+    Returns two one-dimensional arrays of coordinates, one for x, one for y.
+    These are expressed in the wafer referential.The origin is placed at the
+    centre of the image. Extent and units are determined by 'width' and
+    'height'.
+
+    ARGUMENTS
+    array (2D numpy array) - array of 1s and 0s, as returned by path_to_array.
+        Black pixels are 0s.
+    width (float) - width of the image in the desired units.
+    height (float) - height of the image in the desired units.
+
+    '''
+
+    # Create coordinates
+    Ny, Nx = data.shape
+    x = linspace(-width/2, width/2, Nx)
+    y = linspace(height/2, -height/2, Ny) # note inverted signs, y is upwards
+    xx, yy = meshgrid(x, y)
+
+    # Find black pixels
+    black = data == 0
+    black_x = xx[black]
+    black_y = yy[black]
+
+    return black_x, black_y # no need to flatten apparently
+
+def transform_coordinates(x, y, z, O, eX, eY, eZ):
+    ''' Return stage coordinates X, Y, Z from wafer coordinates x, y, z.
+
+    x, y, and z must be the same shape. Point O and orthonormal basis eX,
+    eY, eZ define the origin and orientation of the wafer coordinate system,
+    expressed in stage coordinates. The function returns array X, Y, and Z of
+    same shape as x, y, and z.
+
+    ARGUMENTS
+    x, y, z (numpy arrays) - points to be transformed
+    O (Vector) - origin of the wafer coordinate system
+    eX, eY, eZ (Vector) - orthonormal basis defining the wafer orientation
+
+    RETURNS
+    X, Y, Z (numpy arrays) - coordinates in the stage referential.
+
+    '''
+
+    X = O[0] + x * eX[0] + y * eY[0] + z * eZ[0]
+    Y = O[1] + x * eX[1] + y * eY[1] + z * eZ[1]
+    Z = O[2] + x * eX[2] + y * eY[2] + z * eZ[2]
+
+    return X, Y, Z
 
 if __name__ == '__main__':
-    O = array([0, 0, 0])
-    A = array([1, 0, 0])
-    B = array([0, 1, 0])
 
-    print get_referential(O, A, B)
+    arr = array([[1, 1, 1],
+                 [0, 1, 1],
+                 [1, 0, 1]])
+
+    width = 2
+    height = 4
+
+    x, y = get_black_points(arr, width, height) 
+    print x, y
--- test_toolkit.py
+++ test_toolkit.py
@@ -61,3 +61,49 @@ def test_get_referential():
     assert all(eX == array([1, 0, 0]))
     assert all(eY == array([0, 1, 0]))
     assert all(eZ == array([0, 0, 1]))
+
+def test_get_black_points():
+
+    ''' Make sure the proper coordinates are returned by get_black_points() '''
+
+    arr = array([[1, 1, 1],
+                 [0, 1, 1],
+                 [1, 0, 1]])
+
+    width = 2   # extend from -1 to 1 
+    height = 4  # extend from -2 to 2
+
+    x, y = get_black_points(arr, width, height) 
+    
+    assert all(x == array([-1, 0]))
+    assert all(y == array([0, -2]))
+
+def test_transform_coordinates():
+    ''' Make sure the coordinates are transformed right '''
+
+    P1 = array([1, 1, 1])
+    P2 = array([0, 0, 0])
+
+    # Simple translation of the origin
+    O = array([1, 1, 1])
+    eX = array([1, 0, 0])
+    eY = array([0, 1, 0])
+    eZ = array([0, 0, 1])
+
+    X1, Y1, Z1 = transform_coordinates(P1[0], P1[1], P1[2], O, eX, eY, eZ)
+    X2, Y2, Z2 = transform_coordinates(P2[0], P2[1], P2[2], O, eX, eY, eZ)
+
+    assert (X1, Y1, Z1) == (2, 2, 2)
+    assert (X2, Y2, Z2) == (1, 1, 1)
+    
+    # Simple 90 degree of the wafer around ez:
+    O = array([0, 0, 0])
+    eX = array([0, 1, 0])
+    eY = array([-1, 0, 0])
+    eZ = array([0, 0, 1])
+
+    X1, Y1, Z1 = transform_coordinates(P1[0], P1[1], P1[2], O, eX, eY, eZ)
+    X2, Y2, Z2 = transform_coordinates(P2[0], P2[1], P2[2], O, eX, eY, eZ)
+
+    assert (X1, Y1, Z1) == (-1, 1, 1)
+    assert (X2, Y2, Z2) == (0, 0, 0)
